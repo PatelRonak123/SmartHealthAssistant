6e8cca77d92ee2e6de7b18834e229bec4b141e1f
Ronak Patel <email.otherworks@gmail.com>
2025-07-20 21:07:34 +0530

    ongoing messageInput

--- frontend/src/store/slices/chatSlice.js
+++ frontend/src/store/slices/chatSlice.js
@@ -1,53 +1,73 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { axiosInstance } from '../../lib/axios';
-import { toast } from 'react-toastify';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../lib/axios";
+import { toast } from "react-toastify";
 
 export const getUsers = createAsyncThunk(
-    'chat/getUsers',
-    async (_, thunkAPI) => {
-        try {
-            const res = await axiosInstance.get("/message/users");
-            return res.data.users;
-        }catch (error) {
-            toast.error(error.response?.data?.message);
-            return thunkAPI.rejectWithValue(error.response?.data?.message); 
-        }
+  "chat/getUsers",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axiosInstance.get("/message/users");
+      return res.data.users;
+    } catch (error) {
+      toast.error(error.response?.data?.message);
+      return thunkAPI.rejectWithValue(error.response?.data?.message);
     }
+  }
 );
-
+export const getMessages = createAsyncThunk(
+  "chat/getMessages",
+  async (userId, thunkAPI) => {
+    try {
+      const res = await axiosInstance.get(`/messag/${userId}`);
+      return res.data;
+    } catch (error) {
+      toast.error(error.response.data.message);
+      return thunkAPI.rejectWithValue(error.response.data.message);
+    }
+  }
+);
 const chatSlice = createSlice({
-    name: 'chat',
-    initialState: {
-        messages: [],
-        users: [],
-        selectedUser: null,
-        isUsersLoading: false,
-        isMessagesLoading: false,
+  name: "chat",
+  initialState: {
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+  },
+  reducers: {
+    setSelectedUser: (state, action) => {
+      state.selectedUser = action.payload;
     },
-    reducers: {
-        setSelectedUser: (state, action) => {
-            state.selectedUser = action.payload;
-        },
-        pushNewMessage: (state, action) => {
-            state.messages.push(action.payload);
-        },
+    pushNewMessage: (state, action) => {
+      state.messages.push(action.payload);
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getUsers.pending, (state) => {
-                state.isUsersLoading = true;
-            })
-            .addCase(getUsers.fulfilled, (state, action) => {
-                state.users = action.payload;
-                state.isUsersLoading = false;
-            })
-            .addCase(getUsers.rejected, (state) => {
-                state.isUsersLoading = false;
-            });
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUsers.pending, (state) => {
+        state.isUsersLoading = true;
+      })
+      .addCase(getUsers.fulfilled, (state, action) => {
+        state.users = action.payload;
+        state.isUsersLoading = false;
+      })
+      .addCase(getUsers.rejected, (state) => {
+        state.isUsersLoading = false;
+      })
+      .addCase(getMessages.pending, (state) => {
+        state.isMessagesLoading = true;
+      })
+      .addCase(getMessages.fulfilled, (state, action) => {
+        state.messages = action.payload;
+        state.isMessagesLoading = false;
+      })
+      .addCase(getMessages.rejected, (state, action) => {
+        state.isMessagesLoading = false;
+      });
+  },
 });
 
 export const { setSelectedUser, pushNewMessage } = chatSlice.actions;
 
 export default chatSlice.reducer;
-
