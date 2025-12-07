31e5772689458dc13a54dc3c436cd0b8bbf1a58d
Ronak Patel <email.otherworks@gmail.com>
2025-12-07 22:00:15 +0530

    Update axios baseURL for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:4000/api/v1"
+      ? // ? "http://localhost:4000/api/v1"
+        "https://smarthealthassistant-2.onrender.com/"
       : "/",
   withCredentials: true,
 });
